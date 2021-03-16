487d3ed2f81938c65dc63d3625f7de74753c29ad
Yusuf-Bhabha <56393456+Yusuf-Bhabha@users.noreply.github.com>
2021-03-16 15:00:29 +0000

    class library

---
+++ CarClassLibrary/CarClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class CarClass
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Mileage { get; set; }
+
+        public decimal Price { get; set; }
+
+        public CarClass()
+        {
+            Make = "empty";
+            Model = "empty";
+            Mileage = 0;
+            Price = 0.00m;
+        }
+
+        public CarClass(string a, string b, int c, decimal d)
+        {
+            Make = a;
+            Model = b;
+            Mileage = c;
+            Price = d;
+        }
+
+        override public string ToString()
+        {
+            return "Make: " + Make + " Model: " + Model + " Mileage: " + Mileage + " Price: " + Price;
+        }
+    }
+}
---
+++ CarClassLibrary/StoreClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class StoreClass
+    {
+        public List<CarClass> CarList { get; set; }
+        public List<CarClass> ShoppingList { get; set; }
+
+        public StoreClass()
+        {
+            CarList = new List<CarClass>();
+            ShoppingList = new List<CarClass>();
+        }
+
+        public decimal Checkout()
+        {
+            //initialise the total cost at checkout
+            decimal totalCost = 0;
+
+            //get the cost of shopping list
+            foreach(var c in ShoppingList)
+            {
+                totalCost += c.Price;
+            }
+            //empty the shopping list
+            ShoppingList.Clear();
+
+            //return the total
+            return totalCost;
+        }
+    }
+}
